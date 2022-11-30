29a5e2ff6c3c9c00aaa38eead17dc2379a76b038
Sunil Sapkota <suneelsapkota@gmail.com>
2022-11-30 19:25:07 +0545

    seo update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,53 @@
-import '../styles/globals.css';
 import { ChakraProvider } from "@chakra-ui/react";
-import Layout from '../components/Layout';
+import { NextSeo } from "next-seo";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-
+    <>
+      <NextSeo
+        title="Sunil Sapkota | Full Stack Developer | Devops Engineer | Nepal"
+        description="Sunil Sapkota is a Full Stack Developer and Devops Engineer from Nepal. He is a passionate developer who loves to build things for the web."
+        canonical="https://sunilsapkota.com.np/"
+        openGraph={{
+          url: "https://sunilsapkota.com.np/",
+          title:
+            "Sunil Sapkota | Full Stack Developer | Devops Engineer | Nepal",
+          description:
+            "Sunil Sapkota is a Full Stack Developer and Devops Engineer from Nepal. He is a passionate developer who loves to build things for the web.",
+          images: [
+            {
+              url: "https://sunilsapkota.com.np/sunil.jpg",
+              width: 800,
+              height: 600,
+              alt: "Sunil Sapkota",
+              type: "image/jpeg",
+            },
+            // {
+            //   url: "https://sunilsapkota.com.np/sunil.jpg",
+            //   width: 900,
+            //   height: 800,
+            //   alt: "Sunil Sapkota",
+            //   type: "image/jpeg",
+            // },
+            // { url: "" },
+          ],
+          siteName: "Sunil Sapkota",
+        }}
+        twitter={{
+          handle: "@sunilsapkota",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
-export default MyApp
+export default MyApp;
